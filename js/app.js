f228c1ebd93d824d7dcb69ef5b547b16c00c55f6
siamcse <siamcse01@gmail.com>
2022-09-02 22:57:26 +0600

    details card added

--- js/app.js
+++ js/app.js
@@ -9,9 +9,9 @@ const displayCategory = (categories) =>{
     categories.forEach(category =>{
         // console.log(category);
         const li = document.createElement('li');
-        li.classList.add('hover:bg-sky-400', 'p-2', 'rounded');
+        li.classList.add('hover:bg-sky-400', 'rounded');
         li.innerHTML = `
-            <a onclick="loadDetails(${category.category_id})">${category.category_name}</a>
+            <a onclick="loadDetails(${category.category_id})" class="link link-hover p-2">${category.category_name}</a>
         `;
         categoryContainer.appendChild(li);
     })
@@ -21,5 +21,24 @@ const loadDetails = (detailsId) =>{
     .then(res => res.json())
     .then(data => displayDetails(data.data))
 }
-
+const displayDetails = (details) =>{
+    const detailsContainer = document.getElementById('details-container');
+    detailsContainer.innerHTML = '';
+    details.forEach(detail =>{
+        console.log(detail);
+        const div = document.createElement('div');
+        div.className = ('card lg:card-side bg-base-100 shadow-xl mt-10');
+        div.innerHTML = `
+            <figure><img class="w-60 h-full" src="${detail.image_url}" alt="Album"></figure>
+            <div class="card-body">
+                <h2 class="card-title">${detail.title}</h2>
+                <p>${detail.details.length>500 ? detail.details.slice(0,500)+' ...':detail.details}</p>
+                <div class="card-actions justify-end">
+                    <button class="btn btn-primary">Listen</button>
+                </div>
+            </div>
+        `;
+        detailsContainer.appendChild(div);
+    })
+}
 loadCategory();
